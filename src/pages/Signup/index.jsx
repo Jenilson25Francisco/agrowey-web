a2b7d5d16d886b2de05ea7d33137d80e38070696
Jenilson Francisco <franciscoviwaldo@gmail.com>
2025-04-18 22:23:21 +0100

    refactor: trying to add form validadtion

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -3,8 +3,30 @@ import { Input } from '../../components/input'
 import { Lock, Mail, User } from 'lucide-react'
 import { Button } from '../../components/button'
 import { Container, SigninContainer } from './styles'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const signupFormSchema = z.object({
+  name: z.string().min(1, 'Nome é obrigatório'),
+  email: z.string().email('Email inválido'),
+  password: z.string().min(6, 'Senha deve ter pelo menos 6 caracteres'),
+})
 
 export function Signup() {
+  const { register, handleSubmit, error } = useForm({
+    resolver: zodResolver(signupFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  })
+
+  function formSubmit(data) {
+    console.log(data)
+  }
+
   return (
     <Container>
       <SigninContainer onSubmit={handleSubmit(formSubmit)}>
