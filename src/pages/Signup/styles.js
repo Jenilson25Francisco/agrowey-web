6abc535c45d9770db43ca609e079de016811f95d
Jenilson Francisco <farnciscoviwaldo@gmail.com>
2025-04-22 13:07:39 +0100

    reafctor: modifyng styles of register page

--- src/pages/Signup/styles.js
+++ src/pages/Signup/styles.js
@@ -1,29 +1,31 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
- height: 100dvh;
-
-img {
-  display: none;
-  /* border-top-left-radius: 2.5rem;
-  border-bottom-left-radius: 2.5rem; */
-}
-
-@media (min-width: 768px) {
-  
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   width: 100%;
+  height: 100dvh;
 
   img {
-    display: inline;
-    width: 48.75rem;
-    height: 100dvh;
-    object-fit: cover;
-    object-position: center;
+    display: none;
+    /* border-top-left-radius: 2.5rem;
+    border-bottom-left-radius: 2.5rem; */
   }
-}
+
+  @media (min-width: 768px) {
+    
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+
+    img {
+      display: inline;
+      width: 48.75rem;
+      height: 100dvh;
+      object-fit: cover;
+      object-position: center;
+    }
+
+  }
 `
 
 export const SigninContainer = styled.form`
@@ -31,7 +33,15 @@ export const SigninContainer = styled.form`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  width: 100%;
+  padding: 0 5rem;
+  /* width: 100%; */
+  height: 100vh;
+  max-width: 25.25rem;
+
+  h1 {
+    font-size: 1.25rem;
+    font-weight: 700;
+  }
 
   > div {
     display: flex;
@@ -40,18 +50,27 @@ export const SigninContainer = styled.form`
     margin-top: 2.5rem;
   }
 
-  h1 {
-    font-size: 2.5rem;
-    font-weight: 700;
-    text-align: left;
-  }
-
   p {
     margin-top: 1.5rem;
     display: flex;
     align-items: center;
     gap: 0.5rem;
     font-size: 0.75rem;
+  }
+
+  @media (min-width: 768px) {
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 48.75rem;
+    height: 100vh;
+
+    h1 {
+      font-size: 2.5rem;
+      font-weight: 700;
+    }
+
   }
 
   `
